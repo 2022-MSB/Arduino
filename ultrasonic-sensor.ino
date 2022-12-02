35f974a35da51a816fea46773a77cedcf159741d
chan._ <83104885+soft0725@users.noreply.github.com>
2022-12-02 18:30:05 +0900

    초음파 센서 평균 값을 구하는 코드

--- ultrasonic-sensor.ino
+++ ultrasonic-sensor.ino
@@ -1,6 +1,9 @@
 #include <afstandssensor.h>
 
+#define n 300
+
 AfstandsSensor afstandssensor(13, 12);
+
 
 //초음파 센서의 핀번호를 설정한다.
 int echoPin = 12;
@@ -11,24 +14,23 @@ void setup() {
   // trig를 출력모드로 설정, echo를 입력모드로 설정
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  float duration, distance;
+  float sum = 0;
+  
+  // 초음파를 보낸다. 다 보내면 echo가 HIGH 상태로 대기하게 된다.
+  
+  // echoPin 이 HIGH를 유지한 시간을 저장 한다.
+  for(int i=0; i<n; i++){
+    digitalWrite(trigPin, HIGH);
+    delay(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH); 
+    distance = ((float)(340 * duration) / 10000) / 2;  
+    sum += distance;
+  }
+  Serial.print(sum / n);
 }
 
 void loop() {
-  float duration, distance;
-  
-  // 초음파를 보낸다. 다 보내면 echo가 HIGH 상태로 대기하게 된다.
-  digitalWrite(trigPin, HIGH);
-  delay(10);
-  digitalWrite(trigPin, LOW);
-  
-  // echoPin 이 HIGH를 유지한 시간을 저장 한다.
-  duration = pulseIn(echoPin, HIGH); 
-  // HIGH 였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산 한다.
-  distance = ((float)(340 * duration) / 10000) / 2;  
-  
-  Serial.print(distance);
-  Serial.println("cm");
-  Serial.println(afstandssensor.afstandCM());
-  delay(500);
-  // 수정한 값을 출력
 }
